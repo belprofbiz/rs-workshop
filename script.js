8fd340b7b38e7852cf17b0a3a07fa986a59d8a74
belprofbiz <belprofbiz@gmail.com>
2021-12-20 12:38:21 +0300

    add files

---
+++ script.js
@@ -0,0 +1,10 @@
+//$("body").append("<p>Хуй соси обезьяна</p>");
+//$("h1").text("Этот текст скоро исчезнет").slideUp(5000, 0.9).slideDown(1000);
+//var newHeadingText = prompt("Введите новый заголовок:");
+//$("#special").text(newHeadingText);
+var timeUp = function () {
+    alert("Время вышло!");
+};
+setTimeout(timeUp, 3000);
+console.log("12");
+
